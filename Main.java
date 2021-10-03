fc0231c4cab2bef49ac0a65c157f6da9505b376d
celvinanda <58539550+celvinanda@users.noreply.github.com>
2021-10-03 08:18:56 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,224 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        String opsi[] = {"Belah Ketupat","Segitiga","Persegi"};
+
+        System.out.println("1. Penerapan if else dengan penerapan nested\n"+
+                              "   loop untuk for dengan studi kasus bebas.\n");
+
+        System.out.println("Pilihlah Bentuk berikut:");
+        for (int i = 0; i < opsi.length; i++) {
+            System.out.print("Ketik "+(i+1)+" untuk membuat ");
+            System.out.println(opsi[i]);
+        }
+        System.out.print("Silahkan ketik : ");
+        int input = keyboard.nextInt();
+        System.out.print("Tentukan Panjang Titiknya : ");
+        int n = keyboard.nextInt();
+        if (input == 1){
+            System.out.println("Belah Ketupat\nDiagonal 1 = "+(n*2+1)+" titik \nDiagonal 2 = "+(n*2+1)+" titik");
+            for (int i = 0; i <= n; i++) {
+                for (int j = n; j > i; j--) {
+                    System.out.print("   ");
+                }
+                for (int j = 0; j <= i; j++) {
+                    System.out.print("*  ");
+                }
+                for (int j = 0; j < i; j++) {
+                    System.out.print("*  ");
+                }
+                for (int j = n; j > i; j--) {
+                    System.out.print("   ");
+                }
+                System.out.println();
+            }
+            for (int i = 1; i <= n; i++) {
+
+                for (int j = 0; j < i; j++) {
+                    System.out.print("   ");
+                }
+                for (int j = n; j >= i; j--) {
+                    System.out.print("*  ");
+                }
+
+                for (int j = n; j > i; j--) {
+                    System.out.print("*  ");
+                }
+                for (int j = 0; j < i; j++) {
+                    System.out.print("   ");
+                }
+                System.out.println("");
+            }
+        }else if (input == 2){
+            System.out.println("Segitiga\n Alas = "+(n*2+1)+" titik \n Tinggi = "+(n+1)+" titik");
+            for (int i = 0; i <= n; i++) {
+                for (int j = n; j > i; j--) {
+                    System.out.print("  ");
+                }
+                for (int j = 0; j <= i; j++) {
+                    System.out.print("* ");
+                }
+                for (int j = 0; j < i; j++) {
+                    System.out.print("* ");
+                }
+                for (int j = n; j > i; j--) {
+                    System.out.print("  ");
+                }
+                System.out.println();
+            }
+        }else if (input == 3){
+            System.out.println("Persegi\n Sisi = "+(n*2+1)+ " titik");
+            for (int i = 0; i <= n; i++) {
+                for (int j = n; j > i; j--) {
+                    System.out.print("*  ");
+                }
+                for (int j = 0; j <= i; j++) {
+                    System.out.print("*  ");
+                }
+                for (int j = 0; j < i; j++) {
+                    System.out.print("*  ");
+                }
+                for (int j = n; j > i; j--) {
+                    System.out.print("*  ");
+                }
+                System.out.println();
+            }
+            for (int i = 1; i <= n; i++) {
+
+                for (int j = 0; j < i; j++) {
+                    System.out.print("*  ");
+                }
+                for (int j = n; j >= i; j--) {
+                    System.out.print("*  ");
+                }
+
+                for (int j = n; j > i; j--) {
+                    System.out.print("*  ");
+                }
+                for (int j = 0; j < i; j++) {
+                    System.out.print("*  ");
+                }
+                System.out.println("");
+            }
+        }else {
+            System.out.println("Pilihan tidak sesuai");
+        }
+
+        System.out.println("\n\n2. Penerapan switch case dengan for dan while");
+
+        System.out.println("Ketik 1 untuk menghitung Luas");
+        System.out.println("Ketik 2 untuk menghitung Deret Matematika");
+        System.out.print("Pilih salah satu : ");
+        int inputDua = keyboard.nextInt();
+
+        switch (inputDua){
+            case 1:
+                for (int i = 0; i < opsi.length; i++) {
+                    System.out.print("Ketik "+(i+1)+" untuk menghitung luas ");
+                    System.out.println(opsi[i]);
+                }
+                int bentuk = keyboard.nextInt();
+                switch (bentuk){
+                    case 1:
+                        System.out.print("Masukan \nDiagonal = ");
+                        int diagonal1= keyboard.nextInt();
+
+                        System.out.println("Luas = (Diagonal 1 x Diagonal 2)/2");
+                        System.out.println("     = ("+diagonal1+" x "+diagonal1+")/2");
+                        System.out.println("     = (Diagonal "+(diagonal1*diagonal1)+"/2");
+                        System.out.println("     = "+((diagonal1*diagonal1)/2));
+                        break;
+                    case 2:
+                        System.out.print("Masukan \nAlas = ");
+                        int alas= keyboard.nextInt();
+                        System.out.print("Tinggi = ");
+                        int tinggi= keyboard.nextInt();
+
+                        System.out.println("Luas = (Alas x Tinggi)/2");
+                        System.out.println("     = (Alas "+alas+" x Tinggi "+tinggi+")/2");
+                        System.out.println("     = "+(alas*tinggi)+"/2");
+                        System.out.println("     = "+((alas*tinggi)/2));
+                        break;
+                    case 3:
+                        System.out.print("Masukan \nSisi = ");
+                        int sisi= keyboard.nextInt();
+
+                        System.out.println("Luas = Sisi x Sisi");
+                        System.out.println("     = (Sisi "+sisi+" x Sisi "+sisi+")");
+                        System.out.println("     = "+(sisi*sisi));
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case 2:
+                System.out.println("Pilih jenis peritungan Deret Matematika");
+                String deret[] = {"Bilangan Ganjil","Bilangan Genap","Fibonacci"};
+                for (int i = 0; i < deret.length; i++) {
+                    System.out.println("- Tentukan Deret "+deret[i]+ " (Ketik "+(i+1)+")");
+                }
+                System.out.print("Pilih salah satu : ");
+
+
+                int deretAngka = 0;
+                int max = 500;
+                int suku = 0;
+
+                //while (deretAngka > 500 ){
+                    int inputDeret = keyboard.nextInt();
+                    switch (inputDeret){
+                        case 1:
+                            System.out.println();
+                            while (deretAngka <= max){
+                                deretAngka++;
+                                if (deretAngka%2 == 0){
+                                    System.out.print(deretAngka+" ");
+                                    suku++;
+                                }
+
+                            }
+                            System.out.println("\nBilangan diatas adalah bilangan genap dibawah "+max+" dan jumlah suku ada "+suku);
+                            break;
+                        case 2:
+                            System.out.println();
+                            while (deretAngka < max){
+                                deretAngka++;
+                                if (deretAngka%2 != 0){
+                                    System.out.print(deretAngka+" ");
+                                    suku++;
+                                }
+                            }
+                            System.out.println("\nBilangan diatas adalah bilangan ganjil dibawah "+max+" dan jumlah suku ada "+suku);
+
+                            break;
+                        case 3:
+                            System.out.println();
+                            int rumus = 0;
+                            int num1 = 0, num2 = 1;
+
+                            while(num1<max)
+                            {
+                                System.out.print(num1+" ");
+                                rumus = num1 + num2;
+                                num1 = num2;
+                                num2 = rumus;
+                                suku++;
+                            }
+                            System.out.println("\nBilangan diatas adalah bilangan Fibonacci dibawah "+max+" dan jumlah suku ada "+suku);
+
+                            break;
+                    }
+
+                break;
+            default:
+                break;
+        }
+
+    }
+
+}
